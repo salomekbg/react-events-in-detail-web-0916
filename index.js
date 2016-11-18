e3f4c8390121a5cf5b8abef85dc0e4eb7dd34f60
SluvsP <salomekbg@gmail.com>
2016-11-18 06:25:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-const app = "I don't do much.";
+const React = require('react');
+
+class Clicker extends React.Component {
+  constructor() {
+    super();
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    console.log(event.type); // prints 'click'
+  }
+
+  render() {
+    return (
+      hi
+      // <button onClick={this.handleClick}>Click me!</button>
+    );
+  }
+}
+
+module.exports = Clicker;
